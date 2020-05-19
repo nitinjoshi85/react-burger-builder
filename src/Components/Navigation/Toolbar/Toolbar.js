79067de363b428dca5172707a8f5dedac69e1cd5
Nitin Joshi <nitin.joshi@globant.com>
2020-05-19 13:26:15 -0700

    ReadMe updated.

--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import classes from './Toolbar.css';
 import Logo from '../../../Components/Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
@@ -7,7 +9,9 @@ import DrawerToggle from '../../Navigation/SideDrawer/DrawerToggle/DrawerToggle'
 const toolbar = (props) => (
     <header className={classes.Toolbar}> 
         <DrawerToggle clicked={props.clicked}></DrawerToggle>
-        <div className={classes.Logo}><Logo /></div>
+        <div className={classes.Logo}>
+            <Link to="/"> <Logo /> </Link>
+        </div>
         <nav className={classes.DesktopOnly}>
             <NavigationItems isAuthenticated={props.isAuthenticated} />
         </nav>
